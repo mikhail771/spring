c23d3b1ae34acfac680a9248142366b313f0055c
Mikhail771 <mmihajlov771@gmail.com>
2020-10-20 11:17:58 +0300

    deleted unused import

--- src/main/java/spring/config/AppConfig.java
+++ src/main/java/spring/config/AppConfig.java
@@ -3,7 +3,6 @@ package spring.config;
 import java.util.Properties;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
